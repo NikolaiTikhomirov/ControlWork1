ce391cc1e35a2fff6244d8f65ca70eb3403af1dd
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-12-29 23:09:29 +0300

    Writing code which asked the user to fill in the array

---
+++ solutionOption1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вариант решения задачи №1
+
+Console.Write("Какой длинны массив вам потребуется? ");
+bool isParsedFirstArrayLength = int.TryParse(Console.ReadLine(), out int firstArrayLength);
+if(!isParsedFirstArrayLength)
+{
+    Console.WriteLine("Длина массива определяется цифрами! Попробуйте еще раз. :)");
+    return;
+}
+string[] firstArray = new string[firstArrayLength];
+Console.WriteLine("Заполните массив разными строками.");
+for (int i = 0; i < firstArrayLength; i++)
+{
+    firstArray[i] = Console.ReadLine();
+}
