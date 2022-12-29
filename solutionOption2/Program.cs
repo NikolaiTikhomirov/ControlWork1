8d354e221574feddabd5761138bdb295fe8d23ca
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-12-29 23:29:26 +0300

    Write method fill array

--- solutionOption2/Program.cs
+++ solutionOption2/Program.cs
@@ -1,11 +1,28 @@
 ﻿// Вариант решения задачи №2
 
 
-
+Console.Write("Какой длинны массив вам потребуется? ");
+bool isParsedArrayLength = int.TryParse(Console.ReadLine(), out int arrayLength);
+if(!isParsedArrayLength)
+{
+    Console.WriteLine("Длина массива определяется цифрами! Попробуйте еще раз. :)");
+    return;
+}
+string[] firstArray = new string[arrayLength];
+Console.WriteLine("Заполните массив разными строками.");
+FillArray (firstArray);
 
 
 PrintArray(secondArray);
 
+
+void FillArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
 void PrintArray(string[] col)
 {
     Console.WriteLine($"[{string.Join(", ", col)}]");
