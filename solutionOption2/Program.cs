ff804e7eabeddb1459f898e9fcfde5c60b5e5bfc
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-12-29 23:37:19 +0300

    Writing a method that copies strings filtered by length from first array to second

--- solutionOption2/Program.cs
+++ solutionOption2/Program.cs
@@ -11,11 +11,20 @@ if(!isParsedArrayLength)
 string[] firstArray = new string[arrayLength];
 Console.WriteLine("Заполните массив разными строками.");
 FillArray (firstArray);
-
-
+Console.Write("Укажите максимальную длину строк, которые Вы хотите увидеть в новом массиве? ");
+int stringMax = int.Parse(Console.ReadLine());
+string[] secondArray = new string[arrayLength];
+CopyArrayFilteringStringLength(firstArray, secondArray, stringMax);
 PrintArray(secondArray);
 
 
+void CopyArrayFilteringStringLength(string[] fullArray, string[] filteredArray, int filter)
+{
+    for (int i = 0; i < fullArray.Length; i++)
+    {
+        if (fullArray[i].Length <= filter) filteredArray[i] = fullArray[i];
+    }
+}
 void FillArray (string [] array)
 {
     for (int i = 0; i < array.Length; i++)
